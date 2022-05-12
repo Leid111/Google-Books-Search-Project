355ffaf57252109c1f87c3a1a4c8bcf993491f03
Eid Liz <eeid1@woolworths.com.au>
2022-05-12 16:06:34 +1000

    modules added

---
+++ dom-utils.js
@@ -0,0 +1,39 @@
+const createResultsElement = (array) => {
+    const resultsIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let i = 0; i < resultsIndex.length; i++) {
+        const resultNumber = resultsIndex[i];
+        const newElementImage = document.createElement("img");
+        newElementImage.setAttribute("src", array[resultNumber].Image);
+        newElementImage.setAttribute("alt", "na");
+        const parentImage = document.getElementById("results" + resultNumber);
+        parentImage.appendChild(newElementImage);
+
+        const newElementTitle = document.createElement("p");
+        const textNodeTitle = document.createTextNode(
+            `Title: ${array[resultNumber].Title}`
+        );
+        newElementTitle.appendChild(textNodeTitle);
+        const parentTitle = document.getElementById("results" + resultNumber);
+        parentTitle.appendChild(newElementTitle);
+
+        const newElementAuthor = document.createElement("p");
+        const textNodeAuthor = document.createTextNode(
+            `Author: ${array[resultNumber].Author}`
+        );
+        newElementAuthor.appendChild(textNodeAuthor);
+        const parentAuthor = document.getElementById("results" + resultNumber);
+        parentAuthor.appendChild(newElementAuthor);
+
+        const newElementDescripton = document.createElement("p");
+        const textNodeDescripton = document.createTextNode(
+            `Descripton: ${array[resultNumber].Description}`
+        );
+        newElementDescripton.appendChild(textNodeDescripton);
+        const parentDescripton = document.getElementById(
+            "results" + resultNumber
+        );
+        parentDescripton.appendChild(newElementDescripton);
+    }
+};
+
+export default createResultsElement;
